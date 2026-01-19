a3e8f0744bbf8bad954f18b4d9977edbb75ecfa5
NaveenBalagouni <159781517+NaveenBalagouni@users.noreply.github.com>
2026-01-19 21:25:51 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "git_repo_url" {
+  description = "URL of the Git repository containing the OpsMx SSD Helm chart"
+  type        = string
+  default     = "https://github.com/OpsMx/enterprise-ssd.git"
+}
+
+variable "git_branch" {
+  description = "Git branch to clone (can be updated for upgrades)"
+  type        = string
+  default     = "2025-04"
+}
+
+variable "kubeconfig_path" {
+  description = "Path to your kubeconfig file"
+  type        = string
+  default     = "~/snap/kubectl/ssd-use.config"
+}
+
+
+# Ingress Configuration
+# ---------------------------------------------
+variable "ingress_hosts" {
+  description = "The DNS hostname for the SSD UI (must be lowercase)"
+  type        = list(string)
+}
+
+
+variable "namespace" {
+  description = "Kubernetes namespace to deploy SSD"
+  type        = string
+  default     = "ssd-tf"
+}
+
+
+variable "helm_release_name" {
+  description = "Name of the Helm release"
+  type        = string
+  default     = "ssd-opsmx-terraform"
+}
+
+
+
+variable "cert_manager_installed" {
+  description = "Set to true if cert-manager is installed"
+  type        = bool
+  default     = true
+}
