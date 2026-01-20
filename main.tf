2bb4ed9071dcd956df1f225191d72de40f0d5580
NaveenBalagouni <159781517+NaveenBalagouni@users.noreply.github.com>
2026-01-20 16:13:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "helm_release" "opsmx_ssd" {
 
   depends_on = [null_resource.clone_ssd_chart]
 
-  name      = "ssd-terraform"
+  name      = ""ssd-terraform-${each.key}"
   namespace = var.namespace
   chart     = "/tmp/enterprise-ssd/charts/ssd"
   values    = [data.local_file.ssd_values.content]
