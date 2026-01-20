4eb2409d20b0b21cdcc0f37232bca49973ef8490
NaveenBalagouni <159781517+NaveenBalagouni@users.noreply.github.com>
2026-01-20 15:13:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "git_branch" {
 variable "kubeconfig_path" {
   description = "Path to your kubeconfig file"
   type        = string
-  default     = "~/snap/kubectl/ssd-use.config"
+  default     = ""
 }
 
 
